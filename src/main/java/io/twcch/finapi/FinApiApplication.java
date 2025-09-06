9f0db8896e1636d7a65c683f8b988d7a4fc035d4
Alan Hsieh <twcch1218@gmail.com>
2025-09-06 13:42:40 +0800

    Initial commit

---
+++ src/main/java/io/twcch/finapi/FinApiApplication.java
@@ -0,0 +1,13 @@
+package io.twcch.finapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/twcch/finapi/FinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.twcch.finapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
